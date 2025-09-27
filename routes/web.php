d29e0251d31e1b1f5f160d5380789e783a8d88ad
Mohit Mamoria <mohit.mamoria@gmail.com>
2025-09-27 17:53:23 +0530

    moved test routes only for local env

--- routes/web.php
+++ routes/web.php
@@ -15,40 +15,42 @@ Route::get('/', function () {
 Route::get('/webhooks/whatsapp', [WhatsappWebhookController::class, 'verify']);
 Route::post('/webhooks/whatsapp', [WhatsappWebhookController::class, 'handle']);
 
-Route::get('/test', function () {
-    try {
-        $response = Shopify::storefront()->call('storefront/createCart', [
-            'input' => [
-                'buyerIdentity' => [
-                    'phone' => '+919716313713',
-                ],
-                'attributes' => [
-                    ['key' => 'ref', 'value' => 'preorder-campaign'],
-                ],
-                'lines' => [
-                    ['merchandiseId' => "gid://shopify/ProductVariant/45025566195891", 'quantity' => 1],
-                ],
-            ]
-        ]);
+if (app()->environment('local')) {
+    Route::get('/test', function () {
+        try {
+            $response = Shopify::storefront()->call('storefront/createCart', [
+                'input' => [
+                    'buyerIdentity' => [
+                        'phone' => '+919716313713',
+                    ],
+                    'attributes' => [
+                        ['key' => 'ref', 'value' => 'preorder-campaign'],
+                    ],
+                    'lines' => [
+                        ['merchandiseId' => "gid://shopify/ProductVariant/45025566195891", 'quantity' => 1],
+                    ],
+                ]
+            ]);
 
-        $checkoutUrl = data_get($response, 'cartCreate.cart.checkoutUrl');
+            $checkoutUrl = data_get($response, 'cartCreate.cart.checkoutUrl');
 
-        if ($checkoutUrl) {
-            return redirect($checkoutUrl);
+            if ($checkoutUrl) {
+                return redirect($checkoutUrl);
+            }
+
+            dd($response);
+        } catch (ShopifyException $e) {
+            dd($e->context());
         }
+    });
 
-        dd($response);
-    } catch (ShopifyException $e) {
-        dd($e->context());
-    }
-});
+    Route::get('/products/{id}', function ($id) {
+        try {
+            $response = Shopify::admin()->call('admin/getProduct', ['id' => 'gid://shopify/Product/' . $id]);
 
-Route::get('/products/{id}', function ($id) {
-    try {
-        $response = Shopify::admin()->call('admin/getProduct', ['id' => 'gid://shopify/Product/' . $id]);
-
-        dd($response);
-    } catch (ShopifyException $e) {
-        dd($e->context());
-    }
-});
+            dd($response);
+        } catch (ShopifyException $e) {
+            dd($e->context());
+        }
+    });
+}
