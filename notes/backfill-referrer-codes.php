bf627fc3503a990302f2ce5b2b7fffeb2f4f6819
Mohit Mamoria <mohit.mamoria@gmail.com>
2025-09-29 12:15:33 +0530

    fixed note

--- notes/backfill-referrer-codes.php
+++ notes/backfill-referrer-codes.php
@@ -3,5 +3,5 @@
 foreach (App\Models\Player::whereNull('referrer_code')->cursor() as $player) {
     $player->referrer_code = get_unique_referrer_code($player);
     echo sprintf("Player %d: %s\n", $player->id, $player->referrer_code);
-    // $player->save();
+    $player->save();
 }
