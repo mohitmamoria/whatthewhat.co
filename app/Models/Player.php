60de419f199d46c71a7ffc969c838228c508d35c
Mohit Mamoria <mohit.mamoria@gmail.com>
2025-09-29 15:33:25 +0530

    updated players to sync with an empty balance

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -4,21 +4,30 @@ namespace App\Models;
 
 use App\Models\Gamification\HasGamification;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Player extends Model
 {
     use HasGamification;
 
-    protected $fillable = ['name', 'number'];
+    protected $fillable = ['name', 'number', 'referrer_code'];
 
     public static function sync($name, $number)
     {
-        static::upsert([
-            [
-                'name' => $name,
-                'number' => $number,
-                'referrer_code' => get_unique_referrer_code(new static(compact('name', 'number')))
-            ],
-        ], ['number'], ['name']);
+        DB::transaction(function () use ($name, $number) {
+            $player = static::updateOrCreate(
+                ['number' => $number],
+                ['name' => $name]
+            );
+
+            if ($player->wasRecentlyCreated) {
+                // Set a unique referrer code
+                $player->referrer_code = get_unique_referrer_code($player);
+                $player->save();
+
+                // Set an empty balance account
+                $player->balance()->firstOrCreate();
+            }
+        });
     }
 }
