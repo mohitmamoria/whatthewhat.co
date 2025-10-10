40c8c239cecb4ebcfee04b2c92584a4ab4098fc4
Mohit Mamoria <mohit.mamoria@gmail.com>
2025-10-10 15:35:42 +0530

    updated the banner

--- resources/js/partials/GoodiesV1.vue
+++ resources/js/partials/GoodiesV1.vue
@@ -60,7 +60,7 @@
                         <p
                             class="mx-auto inline max-w-xl rounded-full bg-yellow-300 px-4 py-2 font-serif text-sm font-bold text-pretty text-gray-900"
                         >
-                            🚨 LESS THAN 200 LEFT NOW
+                            🚨 LESS THAN 100 LEFT NOW
                         </p>
                     </div>
                 </div>
