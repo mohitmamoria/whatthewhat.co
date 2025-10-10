d79c0fb549bcb971913776fef964d19092fc4027
Mohit Mamoria <mohit.mamoria@gmail.com>
2025-10-10 15:27:35 +0530

    checking failed invites

---
+++ app/Console/Commands/RetryFailedPreorderInvites.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\MessageStatus;
+use App\Models\Message;
+use App\Models\Player;
+use Illuminate\Console\Command;
+
+class RetryFailedPreorderInvites extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:retry-failed-invites {threshold_days=2 : Only retry invites that failed more than this many days ago}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Retry failed preorder invites';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $message = Message::TEMPLATE_PREFIX . 'preorders_invite';
+
+        $daysThreshold = $this->argument('threshold_days');
+        $thresholdDate = now()->subDays($daysThreshold);
+
+        $invitesFailed = Player::whereHas('messages', function ($query) use ($message, $thresholdDate) {
+            $query->where('body->content', $message)
+                ->where('status', MessageStatus::FAILED)
+                ->whereRaw('created_at = (SELECT MAX(created_at) FROM messages WHERE player_id = players.id AND status = ?)', [MessageStatus::FAILED])
+                ->where('created_at', '<', $thresholdDate);
+        })->whereDoesntHave('messages', function ($query) use ($message) {
+            $query->where('body->content', $message)->whereNot('status', MessageStatus::FAILED);
+        })->count();
+
+        $this->info("Total failed invites: $invitesFailed");
+    }
+}
