a7f6783f2eb7823c45c99d9cb11b030081c3b222
Mohit Mamoria <mohit.mamoria@gmail.com>
2025-10-11 22:56:27 +0530

    reusing cart for the visitor

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -7,6 +7,8 @@ use App\Models\Player;
 use App\Models\Product;
 use App\Services\Shopify\Shopify;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 
@@ -48,7 +50,6 @@ class ShopController extends Controller
             'quantity' => ['required', 'integer', 'min:1', 'max:10'],
         ]);
 
-
         // check variant exists
         $product = Product::first();
         $variant = collect($product->variants)->firstWhere('id', $validated['variant']);
@@ -56,6 +57,47 @@ class ShopController extends Controller
             throw ValidationException::withMessages(['variant' => 'Invalid variant selected']);
         }
 
+        $cachedCart = Cache::get($this->getCartCacheKey());
+        if ($cachedCart) {
+            $cachedCart = json_decode($cachedCart, true);
+
+            $checkoutUrl = $this->reuseCachedCart($cachedCart, $validated['variant'], $validated['quantity']);
+            return Inertia::location($checkoutUrl);
+        } else {
+            $checkoutUrl = $this->createFreshCart($referrer->referrer_code, $validated['variant'], $validated['quantity']);
+            return Inertia::location($checkoutUrl);
+        }
+    }
+
+    private function reuseCachedCart(array $cachedCart, string $variantId, int $quantity): string
+    {
+        // Remove existing lines
+        Shopify::storefront()->call('storefront/removeCartLines', [
+            'cartId' => $cachedCart['id'],
+            'lineIds' => $cachedCart['lines'],
+        ]);
+
+        // Add new lines
+        $response = Shopify::storefront()->call('storefront/addCartLines', [
+            'cartId' => $cachedCart['id'],
+            'lines' => [
+                [
+                    'merchandiseId' => $variantId,
+                    'quantity' => $quantity,
+                ],
+            ],
+        ]);
+
+        // Cache the cart
+        Cache::put($this->getCartCacheKey(), json_encode($this->turnCartCacheable(
+            data_get($response, 'cartLinesAdd'),
+        )), now()->addMinutes(20));
+
+        return data_get($response, 'cartLinesAdd.cart.checkoutUrl');
+    }
+
+    private function createFreshCart(string $ref, string $variantId, int $quantity): string
+    {
         $response = Shopify::storefront()->call('storefront/createCart', [
             'input' => [
                 'attributes' => [
@@ -63,15 +105,50 @@ class ShopController extends Controller
                 ],
                 'lines' => [
                     [
-                        'merchandiseId' => $validated['variant'],
-                        'quantity' => $validated['quantity']
+                        'merchandiseId' => $variantId,
+                        'quantity' => $quantity
                     ],
                 ],
             ]
         ]);
 
-        $checkoutUrl = data_get($response, 'cartCreate.cart.checkoutUrl');
+        // Cache the cart
+        Cache::put($this->getCartCacheKey(), json_encode($this->turnCartCacheable(
+            data_get($response, 'cartCreate'),
+        )), now()->addMinutes(20));
 
-        return Inertia::location($checkoutUrl);
+        return data_get($response, 'cartCreate.cart.checkoutUrl');
+    }
+
+    protected function turnCartCacheable($shopifyCart)
+    {
+        /**
+         * SCHEMA:
+         *
+         * $cart = [
+         * 'id' => 'ID',
+         * 'checkoutUrl' => 'URL'
+         * 'lines' => ['ID1', 'ID2']
+         * ]
+         */
+        return [
+            'id' => data_get($shopifyCart, 'cart.id'),
+            'checkoutUrl' => data_get($shopifyCart, 'cart.checkoutUrl'),
+            'lines' => collect(data_get($shopifyCart, 'cart.lines.edges'))->pluck('node.id'),
+        ];
+    }
+
+    private function getCartCacheKey()
+    {
+        return 'cart:' . $this->getVisitorId();
+    }
+
+    private function getVisitorId()
+    {
+        if (!session()->has('visitor_id')) {
+            session()->put('visitor_id', (string) Str::uuid());
+        }
+
+        return session()->get('visitor_id');
     }
 }
