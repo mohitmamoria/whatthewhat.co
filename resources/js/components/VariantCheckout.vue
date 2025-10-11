f5e1b7a18399c6103f2d6b63be3e768165d72432
Mohit Mamoria <mohit.mamoria@gmail.com>
2025-10-11 21:34:56 +0530

    added disabled button when checking out

--- resources/js/components/VariantCheckout.vue
+++ resources/js/components/VariantCheckout.vue
@@ -33,6 +33,7 @@ var checkout = () => {
         <button
             type="submit"
             class="mt-4 rounded-md bg-pink-600 px-2.5 py-2 text-sm font-semibold text-white shadow-xs hover:bg-pink-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            :disabled="form.processing"
         >
             Buy {{ form.quantity }} for ₹{{ form.quantity * variant.price }} &rarr;
         </button>
