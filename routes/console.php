27952fd915734fcd3e7c4b572ec4a9c9ac7c7fb0
Mohit Mamoria <mohit.mamoria@gmail.com>
2025-10-24 10:47:20 +0530

    outputting logs to a file

--- routes/console.php
+++ routes/console.php
@@ -12,6 +12,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command(InvitePlayerToPreorder::class, ['1000'])->hourly()->withoutOverlapping();
-Schedule::command(SendUnsentOrderConfirmations::class)->everyMinute()->withoutOverlapping();
-Schedule::command(ProcessPendingGiftPurchases::class)->everyMinute()->withoutOverlapping();
+Schedule::command(InvitePlayerToPreorder::class, ['1000'])->hourly()->sendOutputTo(storage_path('logs/invite_player_to_preorder.log'));
+Schedule::command(SendUnsentOrderConfirmations::class)->everyMinute()->sendOutputTo(storage_path('logs/send_unsent_order_confirmations.log'));
+Schedule::command(ProcessPendingGiftPurchases::class)->everyMinute()->sendOutputTo(storage_path('logs/process_pending_gift_purchases.log'));
