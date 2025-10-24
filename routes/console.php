27718a10c7a1f34c21a88e36b65355b89ae0cf35
Mohit Mamoria <mohit.mamoria@gmail.com>
2025-10-24 10:56:23 +0530

    appended output

--- routes/console.php
+++ routes/console.php
@@ -12,6 +12,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command(InvitePlayerToPreorder::class, ['1000'])->hourly()->sendOutputTo(storage_path('logs/invite_player_to_preorder.log'));
-Schedule::command(SendUnsentOrderConfirmations::class)->everyMinute()->sendOutputTo(storage_path('logs/send_unsent_order_confirmations.log'));
-Schedule::command(ProcessPendingGiftPurchases::class)->everyMinute()->sendOutputTo(storage_path('logs/process_pending_gift_purchases.log'));
+Schedule::command(InvitePlayerToPreorder::class, ['1000'])->hourly()->appendOutputTo(storage_path('logs/invite_player_to_preorder.log'));
+Schedule::command(SendUnsentOrderConfirmations::class)->everyMinute()->appendOutputTo(storage_path('logs/send_unsent_order_confirmations.log'));
+Schedule::command(ProcessPendingGiftPurchases::class)->everyMinute()->appendOutputTo(storage_path('logs/process_pending_gift_purchases.log'));
