90b0680a5e48370398d96bd2a4615c6f4953b755
Mohit Mamoria <mohit.mamoria@gmail.com>
2025-11-23 23:26:22 +0530

    added amazon affliate link

--- resources/js/partials/SoldOut.vue
+++ resources/js/partials/SoldOut.vue
@@ -13,7 +13,7 @@
         <div class="px-4 py-4 text-right sm:px-6">
             <!-- Content goes here -->
             <a
-                href="https://amzn.in/d/2SCwDkI"
+                href="https://wtw.is/amazon"
                 target="_blank"
                 class="inline-block rounded-md bg-pink-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs hover:bg-pink-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600"
             >
--- resources/js/partials/Sticky.vue
+++ resources/js/partials/Sticky.vue
@@ -2,7 +2,7 @@
     <div class="pointer-events-none fixed inset-x-0 bottom-0 sm:flex sm:justify-center sm:px-6 sm:pb-5 lg:px-8">
         <div class="pointer-events-auto flex items-center justify-between gap-x-6 bg-pink-600 px-6 py-2.5 sm:rounded-xl sm:py-3 sm:pr-3.5 sm:pl-4">
             <p class="text-sm/6 text-white">
-                <a href="https://amzn.in/d/2SCwDkI" target="_blank">
+                <a href="https://wtw.is/amazon" target="_blank">
                     <strong class="font-semibold">Available now on Amazon</strong>
                     <svg viewBox="0 0 2 2" class="mx-2 hidden size-0.5 fill-current sm:inline" aria-hidden="true"><circle cx="1" cy="1" r="1" /></svg>
                     <br class="inline sm:hidden" />
