8119c3488119bff3553a48f13f4e0ec8a68cd234
Mohit Mamoria <mohit.mamoria@gmail.com>
2025-12-09 20:51:00 +0530

    made splash screens

--- resources/js/pages/Qotd/Index.vue
+++ resources/js/pages/Qotd/Index.vue
@@ -1,6 +1,8 @@
 <script setup>
 import Qotd from '@/layouts/Qotd.vue';
-import { Link, useForm, usePage } from '@inertiajs/vue3';
+import QotdForGuest from '@/partials/Qotd/QotdForGuest.vue';
+import QotdPreGame from '@/partials/Qotd/QotdPreGame.vue';
+import { useForm, usePage } from '@inertiajs/vue3';
 
 const props = defineProps({
     question: {
@@ -15,11 +17,6 @@ const props = defineProps({
 
 const loggedInPlayer = usePage().props.auth.player;
 
-const form = useForm({});
-const attempt = () => {
-    form.post(route('qotd.attempts', { question: props.question.name }));
-};
-
 const joinForm = useForm({
     name: loggedInPlayer ? loggedInPlayer.name : '',
 });
@@ -31,11 +28,7 @@ const join = () => {
 <template>
     <Qotd>
         <div v-if="!loggedInPlayer" class="mt-10 flex items-center justify-center gap-x-6">
-            <Link
-                :href="route('auth.player.login', { next: route('qotd.index') })"
-                class="rounded-md bg-gray-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-gray-900 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-800"
-                >Login using WhatsApp to play &rarr;</Link
-            >
+            <QotdForGuest />
         </div>
         <div v-else-if="!qotd_game">
             <p class="mb-4 text-gray-700">
@@ -69,15 +62,7 @@ const join = () => {
             </form>
         </div>
         <div v-else>
-            <form @submit.prevent="attempt">
-                <button
-                    type="submit"
-                    class="mt-4 rounded-md bg-pink-600 px-2.5 py-2 text-sm font-semibold text-white shadow-xs hover:bg-pink-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-700"
-                    :disabled="form.processing"
-                >
-                    Play QOTD &rarr;
-                </button>
-            </form>
+            <QotdPreGame :question="question" />
         </div>
     </Qotd>
 </template>
