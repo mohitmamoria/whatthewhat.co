205e67950735acd50c190f8beb9c2f1b62013b3d
Mohit Mamoria <mohit.mamoria@gmail.com>
2025-12-11 18:52:46 +0530

    added reminder command to cron

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,7 @@ use App\Actions\Qotd\RecalculateQotdStats;
 use App\Console\Commands\InvitePlayerToPreorder;
 use App\Console\Commands\ProcessPendingGiftPurchases;
 use App\Console\Commands\RecalculateQotdStatsAndStreaks;
+use App\Console\Commands\SendQotdReminderToPlayers;
 use App\Console\Commands\SendUnsentGiftConfirmations;
 use App\Console\Commands\SendUnsentOrderConfirmations;
 use Illuminate\Foundation\Inspiring;
@@ -18,4 +19,5 @@ Schedule::command(InvitePlayerToPreorder::class, ['1000'])->hourly()->appendOutp
 Schedule::command(SendUnsentOrderConfirmations::class)->everyMinute()->appendOutputTo(storage_path('logs/send_unsent_order_confirmations.log'));
 Schedule::command(ProcessPendingGiftPurchases::class)->everyMinute()->appendOutputTo(storage_path('logs/process_pending_gift_purchases.log'));
 
+Schedule::command(SendQotdReminderToPlayers::class)->everyThirtyMinutes()->appendOutputTo(storage_path('logs/send_qotd_reminder_to_players.log'));
 Schedule::command(RecalculateQotdStatsAndStreaks::class)->dailyAt('21:00')->appendOutputTo(storage_path('logs/recalculate_qotd_stats_and_streaks.log'));
